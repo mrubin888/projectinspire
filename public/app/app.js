ccbf3a20e90e9032aaed6a24c0d73f9db6995a47
Matt Rubin <mrubin888@gmail.com>
2014-12-29 10:14:55 -0800

    Testing new domain

---
+++ public/app/app.js
@@ -0,0 +1,20 @@
+"use strict"
+
+console.log("App.js");
+
+var FrontierInspirationApp = angular.module("FrontierInspirationApp", ["ui.router"]);
+
+// Declare app level module using dependents
+FrontierInspirationApp.config(function($stateProvider, $urlRouterProvider) {
+
+	$urlRouterProvider.otherwise('/splash');
+	$stateProvider
+		.state('splash', {
+			url: '/splash',
+			templateUrl: 'partials/splash.html'
+		})
+		/*.state('home', {
+			url: '/',
+			templateUrl: 'partials/home.html'
+		})*/;
+});
